add9105ea6873de08fdab9f1641543939f86d77b
Aimee MacDonald <aimeelmacdonald@gmail.com>
2021-08-29 00:34:46 +0200

    ModalLayer

--- src/layers/ModalLayer.js
+++ src/layers/ModalLayer.js
@@ -1,17 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
-export const ModalLayer = () => (
-  <StyledModalLayer>
-    <h1>Modal Layer</h1>
-  </StyledModalLayer>
-)
+export const ModalLayer = () => {
+  const [ isOpen, setOpen ] = useState(false)
+
+  return (
+    <StyledModalLayer isOpen={isOpen}>
+      <h1>Modal Layer</h1>
+    </StyledModalLayer>
+  )
+}
 
 const StyledModalLayer = styled.div`
   position: fixed;
   z-index: 30;
   width: 100%;
-  height: 100%;
+  height: ${({ isOpen }) => isOpen ? '100%' : '0'};
+  overflow: hidden;
   display: grid;
   place-items: center center;
 `
--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ const MainComponent = () => (
     <GlobalStyle/>
     <WorkspaceLayer/>
     <NavigationLayer/>
-    {/* <ModalLayer/> */}
+    <ModalLayer/>
   </StyledMainComponent>
 )
 
