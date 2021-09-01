0af4546728fe1cca0354932ffc5ca2e42842236e
Aimee MacDonald <aimeelmacdonald@gmail.com>
2021-09-01 22:46:39 +0200

    Styled lists

--- src/workspaces/Lists/List/List.js
+++ src/workspaces/Lists/List/List.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
 export const List = ({ title, items, addListItem, destroyListItem, destroyList }) => {
+  const [ isOpen, setOpen ] = useState(false)
+
   const addItem = e => {
     e.preventDefault()
     addListItem(e.target.newItem.value)
@@ -9,15 +11,21 @@ export const List = ({ title, items, addListItem, destroyListItem, destroyList }
   }
 
   return (
-    <StyledList>
-      <h1>{ title }</h1>
-      <button onClick={destroyList}>X</button>
+    <StyledList
+      isOpen={isOpen}
+      onMouseEnter={() => setOpen(true)}
+      onMouseLeave={() => setOpen(false)}
+      >
+      <div className='listHeader'>
+        <h1>{ title }</h1>
+        <button onClick={destroyList}></button>
+      </div>
 
       <ul>
-        {items && items.map(listItem => (
-          <li key={listItem._id}>
+        {items && items.map((listItem, i) => (
+          <li key={listItem._id} className={`${!(i % 2) ? 'odd' : 'even'}Row`}>
             <p>{ listItem.value }</p>
-            <button onClick={() => destroyListItem(listItem._id)}>X</button>
+            <button onClick={() => destroyListItem(listItem._id)}></button>
           </li>
         ))}
       </ul>
@@ -31,7 +39,56 @@ export const List = ({ title, items, addListItem, destroyListItem, destroyList }
 }
 
 const StyledList = styled.div`
-  background-color: green;
+  background-color: ${({ isOpen }) => isOpen ? 'green' : 'purple'};
+  padding: ${({ isOpen }) => isOpen ? '1.6' : '0.4'}rem;
+  border-radius: 0.4rem;
+  transition: 0.4s;
+
+  .listHeader, li {
+    display: flex;
+    justify-content: space-between;
+    align-items: ${({ isOpen }) => isOpen ? 'flex-start' : 'center'};
+  }
+
+  .listHeader button, li button {
+    background-color: red;
+    width: 1.2rem;
+    height: 1.2rem;
+    border-radius: 50%;
+    cursor: pointer;
+  }
+
+  ul {
+    width: 16rem;
+    height: ${({ isOpen }) => isOpen ? '8rem' : '0'};
+    overflow-y: ${({ isOpen }) => isOpen ? 'scroll' : 'hidden'};
+    margin: ${({ isOpen }) => isOpen ? '1rem 0' : '0'};
+    transition: 0.4s;
+  }
+
+  ul .oddRow{
+    background-color: lime;
+  }
+
+  li {
+    align-items: center;
+    margin: ${({ isOpen }) => isOpen ? '0.1rem 0' : '0'};
+  }
+
+  li button {
+    margin: 0 0.4rem;
+  }
+
+  form {
+    display: flex;
+    height: ${({ isOpen }) => isOpen ? '1.6rem' : '0'};
+    overflow-y: ${({ isOpen }) => isOpen ? 'visible' : 'hidden'};
+    transition: 0.4s;
+  }
+
+  input {
+    width: 100%;
+  }
 `
 
 export default List
